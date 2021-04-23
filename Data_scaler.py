d463ead0e4071b0bc7269883627f50706ef53896
Pkemoi, Dennis (S&T Student) <dpwdb@mst.edu>
2021-04-22 22:04:53 -0500

    Adding files

---
+++ Data_scaler.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.width', 1000)
+save_dir = r"C:\Users\Dennis Pkemoi\Desktop\College Education\2020 NESBE Research internship\Methods and work\Prognostics\Bearing_Dataset\2nd_test.xlsx"
+save_dir1 = r"C:\Users\Dennis Pkemoi\Desktop\College Education\2020 NESBE Research internship\Methods and work\Prognostics\Bearing_Dataset\3rd_test.xlsx"
+
+
+def scaler(path):
+    
