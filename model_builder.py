0392df980cf8d1e5dc2efdfd74c2b7dd425d870f
Dennis Powon <79292641+dpowon9@users.noreply.github.com>
2021-04-22 22:13:32 -0500

    Update model_builder.py

---
+++ model_builder.py
@@ -0,0 +1,19 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+
+
+def rul_LSTM(seq_length, features, out_length):
+    """
+    :param seq_length: lookback
+    :param features: Number of features
+    :param out_length: Output dimensions
+    :return: Model
+    """
+    model = Sequential()
+    model.add(LSTM(input_shape=(seq_length, features), units=100, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=out_length, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
