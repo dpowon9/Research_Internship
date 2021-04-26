56754bfc3b675c9d2e76547a6ec50fe080a9fa49
Pkemoi, Dennis (S&T Student) <dpwdb@mst.edu>
2021-04-26 12:02:27 -0500

    train updates

--- model_builder.py
+++ model_builder.py
@@ -1,6 +1,6 @@
 import keras.optimizers
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
+from keras.layers import Dense, Dropout, LSTM, Bidirectional, Flatten
 
 
 def rul_LSTM(seq_length, features, out_length):
@@ -17,6 +17,25 @@ def rul_LSTM(seq_length, features, out_length):
     model.add(LSTM(units=50, return_sequences=False))
     model.add(Dropout(0.2))
     model.add(Dense(units=out_length, activation='sigmoid'))
-    opt = keras.optimizers.Adam(learning_rate=0.0001)
+    opt = keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(loss='mean_squared_logarithmic_error', optimizer=opt, metrics=['mean_squared_logarithmic_error'])
+    return model
+
+
+def Bi_LSTM(seq_length, features, out_length):
+    """
+    :param seq_length: lookback
+    :param features: Number of features
+    :param out_length: Output dimensions
+    :return: Model
+    """
+    model = Sequential()
+    model.add(Bidirectional(LSTM(units=100, return_sequences=True, dropout=0.2, recurrent_dropout=0.2), input_shape=(seq_length, features)))
+    model.add(Bidirectional(LSTM(units=50, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)))
+    model.add(Bidirectional(LSTM(units=50, return_sequences=True)))
+    model.add(Flatten())
+    model.add(Dense(8))
+    model.add(Dense(units=out_length, activation='sigmoid'))
+    opt = keras.optimizers.Adam(learning_rate=0.001)
     model.compile(loss='mean_squared_logarithmic_error', optimizer=opt, metrics=['mean_squared_logarithmic_error'])
     return model
--- trainer.py
+++ trainer.py
@@ -1,5 +1,5 @@
 import keras
-from model_builder import rul_LSTM
+from model_builder import Bi_LSTM
 from data_ops import arr_generator
 import pandas as pd
 import numpy as np
@@ -33,34 +33,44 @@ cols = [cols[i:i + 11] for i in range(0, len(cols), 11)]
 '''
 for i in range(len(cols)):
     train_df.plot(x='cycles', y=cols[i], subplots=True, xlim=[lim[0], lim[steps-1]], figsize=(20, 20))
-    plt.savefig('images/bearing{0}.pdf'.format(i+1))
+    plt.savefig('Train_metrics/Data_Visualization/bearing{0}.pdf'.format(i+1))
 plt.show()
 '''
 # Setting up training requirements
 X_train, Y_train = arr_generator(train_df, 50, 'cycles')
-model = rul_LSTM(50, X_train.shape[2], 1)
+model = Bi_LSTM(50, X_train.shape[2], 1)
+call = keras.callbacks.ModelCheckpoint(filepath='Models/Bi_LSTM.hdf5', verbose=1, save_best_only=True)
 print(model.summary())
-call = keras.callbacks.ModelCheckpoint(filepath='Models/LSTM.hdf5', verbose=1, save_best_only=True)
 # Training
-model.fit(X_train, Y_train, epochs=100, batch_size=64, validation_split=0.05, verbose=1, callbacks=call)
+model.fit(X_train, Y_train, epochs=20, batch_size=100, validation_split=0.15, verbose=1, callbacks=call)
 # Getting metrics
 ep_loss = model.history.history['loss']
 val_loss = model.history.history['val_loss']
+plt.figure(1)
 plt.plot(range(len(ep_loss)), ep_loss)
 plt.plot(range(len(ep_loss)), val_loss)
 plt.legend(['Training loss', 'Val loss'])
 plt.xlabel('epoch')
 plt.ylabel('mean_squared_logarithmic_error')
 plt.title('Train mean_squared_logarithmic_error vs epoch')
-plt.savefig('Model_metrics/loss.pdf')
+plt.savefig('Train_metrics/loss.pdf')
 # Evaluation
-best_model = keras.models.load_model('Models/LSTM.hdf5')
-performance = best_model.evaluate(X_train, Y_train, batch_size=64)
+best_model = keras.models.load_model('Models/Bi_LSTM.hdf5')
+performance = best_model.evaluate(X_train, Y_train, batch_size=100)
 y_pred = best_model.predict(X_train)
 y_unscaled = y_scaler.inverse_transform(y_pred)
-x, y_true = arr_generator(df, 50, 'cycles')
+y_true = y_scaler.inverse_transform(Y_train.reshape(1, -1))
+y_true = np.array(y_true).flatten()
 final = pd.DataFrame({'Truth': y_true, 'predicted': np.array(y_unscaled).flatten()}, index=np.arange(len(y_pred)))
-final.to_excel('true_pred/train_res.xlsx', index=False)
+final.to_excel('Train_metrics/train_res.xlsx', index=False)
+plt.figure(2)
+plt.plot(y_true, y_true)
+plt.plot(y_true, np.array(y_unscaled).flatten())
+plt.legend(['Ground Truth RUL', 'Predicted RUL'])
+plt.xlabel('RUL in minutes')
+plt.ylabel('RUL in minutes')
+plt.title('GT vs Predicted')
+plt.savefig('Train_metrics/GT_v_Pred.pdf')
 # Model metrics
 mse = mean_squared_error(Y_train, y_pred)
 rmse = np.sqrt(mse)
@@ -72,4 +82,4 @@ scores = [mse, rmse, r2_coef, exp_var, err, log_err]
 col = ['mse', 'rmse', 'r2 score', 'evs', 'max error', 'msle']
 final_res = pd.DataFrame([scores], columns=col)
 print(final_res)
-final_res.to_csv('Model_metrics/eval.csv', index=False)
+final_res.to_csv('Train_metrics/eval.csv', index=False)
